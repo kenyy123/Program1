3f463b0f474f795493a445d5e9b37e3e444abe28
yy <yangyan_clover@163.com>
2017-06-19 00:39:07 +0800

    Jun18

---
+++ Data_Structure/Leetcode/LeetCode03SubstringNoRepeat.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LeetCode03SubstringNoRepeat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public int lengthOfLongestSubstring(String s) {
+	    int i = 0, j = 0, max = 0;
+	    Set<Character> set = new HashSet<>();
+	    
+	    while (j < s.length()) {
+	        if (!set.contains(s.charAt(j))) {
+	            set.add(s.charAt(j++));
+	            max = Math.max(max, set.size());
+	        } else {
+	            set.remove(s.charAt(i++));
+	        }
+	    }
+	    
+	    return max;
+	}
+
+}
