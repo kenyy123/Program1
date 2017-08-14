ad1a854f000dbf98cefbf74faf8832b8f0f17906
yy <yangyan_clover@163.com>
2017-08-15 01:17:51 +0800

    Aug15

--- Data_Structure/Leetcode/Lt21MergeTwoSortedListsa.java
+++ Data_Structure/Leetcode/Lt21MergeTwoSortedListsa.java
@@ -53,4 +53,23 @@ public class Lt21MergeTwoSortedListsa {
         return result.next;
     }
 	
+	//method 2
+	public static ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
+		ListNode fakeHead = new ListNode(0);
+		ListNode current = fakeHead;
+
+		while (l1 != null || l2 != null) {
+			if (l1 == null || (l2 != null && l1.val >= l2.val)) {
+				current.next = l2;
+				current = l2;
+				l2 = l2.next;
+			} else {
+				current.next = l1;
+				current = l1;
+				l1 = l1.next;
+			}
+		}
+		return fakeHead.next;
+	}
+	
 }
