3dfef2eb9a6aca8ebc68a30b05d1a09a6623033a
yy <yangyan_clover@163.com>
2017-08-28 00:59:47 +0800

    Aug28a

---
+++ Data_Structure/common/ds_tree/BinaryTree5.java
@@ -0,0 +1,25 @@
+package ds_tree;
+
+public class BinaryTree5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+//	http://www.cnblogs.com/KeenLeung/archive/2012/11/03/2750545.html
+	class Node
+	{
+	   public int iData ;
+	   public float fData ;
+	   public Node left ;
+	   public Node right ;
+
+	   //·½·¨
+	   public Node(int iData,float fData){}
+	   public void displayNode(){} 
+	}
+	
+	
+
+}
