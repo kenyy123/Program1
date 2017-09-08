b7d10cfefcaa61ae89496de2ae605807291c1344
kenyy123 <yangyan_clover@163.com>
2017-09-08 15:17:12 +0800

    Sep08

---
+++ Data_Structure/common/ds_tree/Java_avl_git_code/NodeAlreadyExistsException.java
@@ -0,0 +1,12 @@
+public class NodeAlreadyExistsException extends Exception {
+
+	public static final String MESSAGE = "A Node already exists that contains the int value: ";
+
+	public NodeAlreadyExistsException() {
+
+	}
+
+	public NodeAlreadyExistsException(String message) {
+		super(message);
+	}
+}
